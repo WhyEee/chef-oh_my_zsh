709677d2b2b2e1fb3c46a69fd28b939d39f38630
WhyEee <yannick.eckey@googlemail.com>
2014-08-07 01:37:29 +0200

    Update and rename ohmyzsh.rb to oh_my_zsh.rb

---
+++ attributes/oh_my_zsh.rb
@@ -0,0 +1,9 @@
+default.oh_my_zsh.theme = "robbyrussell"
+
+default.oh_my_zsh.case_sensitive = false
+default.oh_my_zsh.auto_update = true
+default.oh_my_zsh.update_prompt = true
+default.oh_my_zsh.ls_colors = true
+default.oh_my_zsh.auto_title = true
+
+default.oh_my_zsh.plugins = %w(git)
